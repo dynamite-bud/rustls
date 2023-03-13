d4f336952061d9d42074bd97085dbcb9a043e075
Daniel McCarney <daniel@binaryparadox.net>
2023-03-13 11:00:10 -0400

    crypto: separate module dir, ring sub-module.
    For better code organization this commit moves the generic crypto
    interface code from `src/crypto.rs` to `src/crypto/lib.rs`.
    
    The *ring* specific code implementing the generic interfaces is moved to
    `src/crypto/ring.rs` as a sub-module of `crypto. All imports are
    adjusted accordingly.
    
    This has the advantage of leaving `src/crypto/lib.rs` small, and without
    any *ring* specific imports. In the future we may choose to feature-gate
    the ring sub-module to allow building the crate without a dependency on
    ring.

---
+++ rustls/src/crypto/mod.rs
@@ -0,0 +1,17 @@
+use crate::rand::GetRandomFailed;
+use crate::server::ProducesTickets;
+
+/// *ring* based CryptoProvider.
+pub mod ring;
+
+/// Pluggable crypto galore.
+pub trait CryptoProvider: Send + Sync + 'static {
+    /// Build a ticket generator.
+    fn ticket_generator() -> Result<Box<dyn ProducesTickets>, GetRandomFailed>;
+
+    /// Fill the given buffer with random bytes.
+    fn fill_random(buf: &mut [u8]) -> Result<(), GetRandomFailed>;
+
+    /// Verify that the two input slices are equal, in constant time.
+    fn verify_equal_ct(a: &[u8], b: &[u8]) -> bool;
+}
